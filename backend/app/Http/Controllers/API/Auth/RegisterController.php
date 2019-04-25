c78e21e96c56803c0d99eac9cd907b9113a0204c
teerayut041995 <teerayut041995@gmail.com>
2019-04-25 15:28:43 +0700

    create register controller

---
+++ backend/app/Http/Controllers/API/Auth/RegisterController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+Use App\User;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+    	$user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+    	return $user;
+    }
+}
